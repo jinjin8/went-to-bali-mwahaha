d1bdc15a8a23546c034d8f383188eed43c3a2396
jin camou <jincamou@gmail.com>
2017-07-14 13:49:09 -0700

    use AJAX to show product details on index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,10 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js 
+    end
   end
 
   def new
@@ -25,6 +29,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 end

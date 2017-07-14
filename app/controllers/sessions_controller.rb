06cf658540fb9f5370ca885718c31812fec0f04e
jin camou <jincamou@gmail.com>
2017-07-14 14:43:48 -0700

    add flash msgs for signing up/in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      flash[:notice] = "You're signed in."
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:alert] = "There was a problem signing in. Please try again."
       redirect_to '/sign_in'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "You're signed out."
     redirect_to '/sign_in'
   end
 
